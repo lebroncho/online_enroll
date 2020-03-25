ba02d9dd3f07cdeffb984c40c1d88a8bae6e7c5e
jec360 <jec.farolan@gmail.com>
2020-03-25 14:59:59 +0800

    2nd commit with design

---
+++ resources/views/partials/menu.blade.php
@@ -0,0 +1,63 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4" style="min-height: 917px;">
+    <!-- Brand Logo -->
+    <a href="#" class="brand-link">
+        <span class="brand-text font-weight-light">PWC ENROLLMENT REPORTS</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+        <!-- Sidebar user (optional) -->
+
+        <!-- Sidebar Menu -->
+        <nav class="mt-2">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                <li class="nav-item">
+                    <a href="{{ url('/dash') }}" class="nav-link">
+                        <p>
+                            <i class="fas fa-fw fa-tachometer-alt">
+
+                            </i>
+                            <span>Dashboard</span>
+                        </p>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <p>
+                            <i class="fas fa-chart-bar"></i>
+
+                            </i>
+                            <span>Basic Education</span>
+                        </p>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <p>
+                            <i class="fas fa-chart-bar"></i>
+
+                            </i>
+                            <span>Senior High School</span>
+                        </p>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <p>
+                            <i class="fas fa-chart-bar"></i>
+
+                            </i>
+                            <span>College</span>
+                        </p>
+                    </a>
+                </li>
+
+            </ul>
+        </nav>
+        <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+</aside>
